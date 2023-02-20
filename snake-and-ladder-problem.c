3fc7457f81e1a52b1c34119b3e2f699e079fe264
ABHINAVGOPALAM <115263478+ABHINAVGOPALAM@users.noreply.github.com>
2023-02-20 22:25:28 +0530

    aaa

--- snake-and-ladder-problem.c
+++ snake-and-ladder-problem.c
@@ -1,3 +1,7 @@
 /*Given a snake and ladder board, find the minimum number of dice throws required to reach the destination or last cell from source or 1st cell. This is done by 
 considering that the player can determine which number appears in the dice being biased. The player rolls the dice and if reaches a base of a ladder then he can move up 
 the ladder to a different position/cell and if he reaches a snake then it brings him down away from the destination cell position.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
