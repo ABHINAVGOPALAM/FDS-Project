4a92ac9fbc5a2c54dec46bcf3f5ffa6583737902
ABHINAVGOPALAM <115263478+ABHINAVGOPALAM@users.noreply.github.com>
2023-02-21 20:19:37 +0530

    Update snake-and-ladder-problem.c

--- snake-and-ladder-problem.c
+++ snake-and-ladder-problem.c
@@ -4,13 +4,54 @@ the ladder to a different position/cell and if he reaches a snake then it brings
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#define max 100
+int no_of_nodes, i, j, ns, end, start, a, nl,star,en,x;
+int queue[max],front=-1,rear=-1,new;
+int parent[101];
+int min_no_of_rolls[101];
+int abhi=1;
 struct node
 {
     int data;
     struct node *link;
 };
-int no_of_nodes, i, j, ns, end, start, a, nl,star,en;
+int dequeue()
+{
+    int y;
+    if(front==-1)
+    printf("\nunderflow\n");
+    else
+    {
+     y=queue[front];
+     if(front==rear)
+     {
+        front=-1;
+        rear=-1;
+     }
+     else
+    {
+      front++;
+    }
+    return y;
+    }
+}
+void enqueue(int x)
+{
+    if(rear==max-1)
+    printf("overflow\n");
+    else
+    {
+        if(rear==-1 && front==-1 )
+        {
+            front=0;
+            rear=0;
+        }
+        else
+        rear++;
+        queue[rear]=x;
+    }  
+}
+
 void display_graph(struct node *board[])
 {
     struct node *temp;
@@ -26,8 +67,52 @@ void display_graph(struct node *board[])
         printf("\n");
     }
 }
-// void bfs(struct node *board[]);
-// void path();
+
+
+void bfs(int b,struct node *board[])
+{
+     for(j=0;j<=no_of_nodes+1;j++)
+    {
+        parent[j]=-1;
+        min_no_of_rolls[j]=-1;
+    }
+
+    min_no_of_rolls[b] = 0;
+    enqueue(b);
+    while(front<=rear)
+    {
+        new=queue[front];
+
+        struct node *temp1;
+        temp1=board[new];
+        printf("%d\t",queue[front++]);
+        while(temp1!=NULL)
+        {
+            queue[++rear]=temp1->data;
+            temp1=temp1->link;
+            
+        }
+        while(temp1->link!=NULL)
+        {
+            int p=temp1->data;
+            if(min_no_of_rolls[p]==-1)
+            {
+                min_no_of_rolls[p]=min_no_of_rolls[new]+1;
+                parent[p]=new;
+                enqueue(p);
+            }
+            temp1=temp1->link;
+        }
+        int jaya=dequeue();
+    }
+    printf("%d\n",min_no_of_rolls[98]);
+    printf("%d\n",min_no_of_rolls[99]);
+    printf("%d\n",min_no_of_rolls[101]);
+}
+void path()
+{
+    
+}
 void snakes(int start, int end, struct node *board[])
 {
     for (a = start - 1; a >= start - 6 && a > 0; a--)
@@ -108,5 +193,11 @@ void main()
     }
 
     display_graph(board);
+
+    
+   bfs(abhi,board);
+
+    
 }
+
 
